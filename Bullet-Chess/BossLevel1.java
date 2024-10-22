6a965928010a1e49f3381a4cf4dfb83981a3bc81
Lucas Coveyduck <2478812@edu.vaniercollege.qc.ca>
2024-10-22 09:22:22 -0400

    Added Game Assets Folder

---
+++ Bullet-Chess/BossLevel1.java
@@ -0,0 +1,19 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class BossLevel1 extends World
+{
+
+    /**
+     * Constructor for objects of class BossLevel1.
+     */
+    public BossLevel1()
+    {
+        super(600, 400, 1);
+    }
+}
