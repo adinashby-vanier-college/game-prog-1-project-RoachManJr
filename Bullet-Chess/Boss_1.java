fbfad65e91598b776ca464c587c76a9c867853c6
Ehsan Hussain <6274100@edu.vaniercollege.qc.ca>
2024-11-04 15:13:12 -0500

    horse moves

--- Bullet-Chess/Boss_1.java
+++ Bullet-Chess/Boss_1.java
@@ -7,11 +7,17 @@ import greenfoot.*;
 public class Boss_1 extends Actor
 {
     private BossProjectileHorse bossprojectilehorse; 
+    private int x;
+    private int y;
     private float radius = 75;    
     private float speed = 2;       
-    int move1Delay = 2000;
+    int move1Delay = 1000;
     int cooldownCounter1 = 0;
+    private int step = 0;
+    private int direction;
     
+    int moveDelay = 50;
+    int cooldownCounter = 0;
     /**
      * Act - do whatever the Boss_1 wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
@@ -24,19 +30,47 @@ public class Boss_1 extends Actor
     if (cooldownCounter1 > 0) {
             cooldownCounter1--;
         }
-        moveKnight();
-        
+    if (bossprojectilehorse != null) {
+            bossprojectilehorse.updateCenter(getX(), getY());
+        }
+    if (cooldownCounter<= 0) {
+            moveKnight();
+            cooldownCounter= moveDelay;
+        }
+    if (cooldownCounter > 0) {
+            cooldownCounter--;
+        } 
     }
-
     /**
      * 
      */
     public void moveKnight()
     {
-    }
-    /**
-     * 
-     */
+        if (step == 0) {
+            direction = Greenfoot.getRandomNumber(4); 
+            if (direction == 0) {
+                move(100); 
+            } else if (direction == 1) {
+                move(-100); 
+            } else if (direction == 2) {
+                setLocation(getX(), getY() - 100); 
+            } else if (direction == 3) {
+                setLocation(getX(), getY() + 100); 
+            }
+            step++;
+        } else if (step == 1) {
+            // Move 1 step perpendicular to the previous movement
+            if (direction == 0 || direction == 1) {
+                setLocation(getX(), getY() + 50); // Move 1 step down/up
+            } else {
+                setLocation(getX()+ 50, getY()); // Move 1 step right/left
+            }
+            step++;
+        } else {
+            
+            step = 0; 
+        }
+    }   
     public void move1()
     {
         if (bossprojectilehorse != null) {
--- Bullet-Chess/Challenge1M.java
+++ Bullet-Chess/Challenge1M.java
@@ -15,5 +15,18 @@ public class Challenge1M extends World
     public Challenge1M()
     {
         super(800, 800, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        RoofedWallWithBanner roofedWallWithBanner =  new  RoofedWallWithBanner();
+        addObject(roofedWallWithBanner, 147, 238);
+        roofedWallWithBanner.setLocation(0, 0);
+        removeObject(roofedWallWithBanner);
     }
 }
